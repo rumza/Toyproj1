8c391197c13f9ce2f20947ecbb9748602391e0fa
RummanSamsudeen <rumman@plexyplanet.org>
2012-10-21 20:43:05 -0400

    [core]change
    checking purpose
    
    refs #13

--- firstwidget.cpp
+++ firstwidget.cpp
@@ -1,15 +1,11 @@
 #include "firstwidget.h"
 
 
-#include <iostream>     //includes the header file iostream
+FirstWidget::FirstWidget(QWidget *parent) :
+    QWidget(parent)
+ 
 
-using namespace std;    //tells the compiler to use the std library
+FirstWidget::~FirstWidget()
+{
+}
 
-int main()              // declares the function main which returns an int
-{                       //tells the program to take everything between here and the close as main
-cout<<"Hello World!\n"; // displays Hello world on the console
-
-
-cin.get();              // stalls the program until a key is pressed
-return 0;               // returns 0 (a 1 would indicate an error
-}                       // closes main
